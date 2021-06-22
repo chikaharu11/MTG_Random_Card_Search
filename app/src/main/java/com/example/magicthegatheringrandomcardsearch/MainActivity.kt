c92ad0541bb518acbd53d59d94406758c847ac82
chikaharu11 <62001773+chikaharu11@users.noreply.github.com>
2021-06-22 13:33:30 +0900

    add web test

--- app/src/main/java/com/example/magicthegatheringrandomcardsearch/MainActivity.kt
+++ app/src/main/java/com/example/magicthegatheringrandomcardsearch/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.magicthegatheringrandomcardsearch
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://scryfall.com/")
+
     }
 }
