21db6602e2665f82bfc81f1de80119dfb6e115cc
chikaharu11 <62001773+chikaharu11@users.noreply.github.com>
2021-06-24 14:07:11 +0900

    add 7 type

--- app/src/main/java/com/example/magicthegatheringrandomcardsearch/MainActivity.kt
+++ app/src/main/java/com/example/magicthegatheringrandomcardsearch/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.magicthegatheringrandomcardsearch
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebResourceRequest
@@ -9,18 +8,19 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var text = ""
-    var text2 = ""
-    var text3 = ""
-    var text4 = ""
-    var text5 = ""
-    var text6 = ""
-    var text7 = ""
+    private var text = ""
+    private var text2 = ""
+    private var text3 = ""
+    private var text4 = ""
+    private var text5 = ""
+    private var text6 = ""
+    private var text7 = ""
+
     @SuppressLint("SetJavaScriptEnabled", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,30 +38,83 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
         }
+
+        val edittext = findViewById<EditText>(R.id.editText)
         val checkbox = findViewById<CheckBox>(R.id.checkBox)
         val checkbox2 = findViewById<CheckBox>(R.id.checkBox2)
+        val checkbox3 = findViewById<CheckBox>(R.id.checkBox3)
+        val checkbox4 = findViewById<CheckBox>(R.id.checkBox4)
+        val checkbox5 = findViewById<CheckBox>(R.id.checkBox5)
+        val checkbox6 = findViewById<CheckBox>(R.id.checkBox6)
+        val checkbox7 = findViewById<CheckBox>(R.id.checkBox7)
+
         checkbox.setOnCheckedChangeListener { _, _ ->
             if (checkbox.isChecked) {
-                text = "t:creature"
-                findViewById<TextView>(R.id.textView).text = "https://scryfall.com/random?q=$text $text2"
+                text = "type:creature or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
             } else {
                 text = ""
-                findViewById<TextView>(R.id.textView).text = "https://scryfall.com/random?q=$text $text2"
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
             }
         }
         checkbox2.setOnCheckedChangeListener { _, _ ->
             if (checkbox2.isChecked) {
-                text2 = "t:planeswalker"
-                findViewById<TextView>(R.id.textView).text = "https://scryfall.com/random?q=$text $text2"
+                text2 = "type:planeswalker or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
             } else {
                 text2 = ""
-                findViewById<TextView>(R.id.textView).text = "https://scryfall.com/random?q=$text $text2"
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            }
+        }
+        checkbox3.setOnCheckedChangeListener { _, _ ->
+            if (checkbox3.isChecked) {
+                text3 = "type:Artifact or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            } else {
+                text3 = ""
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            }
+        }
+        checkbox4.setOnCheckedChangeListener { _, _ ->
+            if (checkbox4.isChecked) {
+                text4 = "type:Instant or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            } else {
+                text4 = ""
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            }
+        }
+        checkbox5.setOnCheckedChangeListener { _, _ ->
+            if (checkbox5.isChecked) {
+                text5 = "type:Sorcery or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            } else {
+                text5 = ""
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            }
+        }
+        checkbox6.setOnCheckedChangeListener { _, _ ->
+            if (checkbox6.isChecked) {
+                text6 = "type:Enchantment or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            } else {
+                text6 = ""
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            }
+        }
+        checkbox7.setOnCheckedChangeListener { _, _ ->
+            if (checkbox7.isChecked) {
+                text7 = "type:Land or "
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
+            } else {
+                text7 = ""
+                edittext.setText("https://scryfall.com/random?q=$text$text2$text3$text4$text5$text6$text7")
             }
         }
 
         findViewById<Button>(R.id.button).setOnClickListener{
             webView.visibility = View.VISIBLE
-            webView.loadUrl("https://scryfall.com/random?q=$text $text2")
+            webView.loadUrl(edittext.text.toString())
         }
         findViewById<Button>(R.id.button2).setOnClickListener{
             webView.visibility = View.INVISIBLE
